ea349f43957488f02767f13b683d1ecd691d1168
Christopher Antonellis <christopher.antonellis@gmail.com>
2017-09-01 20:15:20 -0400

    added version file and license

--- setup.py
+++ setup.py
@@ -1,15 +1,54 @@
+import io
+import os
+import sys
+import shutil
 import subprocess
 import setuptools
 
+
+class PublishCommand(setuptools.Command):
+    """Support setup.py publish."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            shutil.rmtree(os.path.join(here, 'dist'))
+        except FileNotFoundError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
 setuptools.setup(
     name="mongobar",
-    version="0.0.1",
+    version=mongobar.__version__.__version__,
     description="MongoDB Backup and Restore Manager",
+    keywords="mongodb backup restore mongodump mongorestore",
     url="https://github.com/chrisantonellis/mongobar",
 
     author="Christopher Antonellis",
     author_email="christopher.antonellis@gmail.com",
-
+    license="MIT",
     packages=[
         "mongobar"
     ],
@@ -35,5 +74,9 @@ setuptools.setup(
             "green",
             "coverage"
         ],
+    },
+    # $ setup.py publish support.
+    cmdclass={
+        'publish': PublishCommand
     }
 )
