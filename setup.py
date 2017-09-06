b9fb17fb529570f8b14d4a744939a7d9cc90175f
Christopher Antonellis <christopher.antonellis@gmail.com>
2017-09-06 11:30:43 -0400

    moved version from __init__ to __version__ and integrated into setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,17 @@ import shutil
 import subprocess
 import setuptools
 
+# read module version file
+setup_abs_path = os.path.abspath(os.path.dirname(__file__))
+version_abs_path = os.path.join(setup_abs_path, "mongobar", "__version__.py")
+module_metadata = {}
+with open(version_abs_path) as file_handle:
+    exec(file_handle.read(), module_metadata)
+
+
 setuptools.setup(
     name="mongobar",
-    version="0.0.4",
+    version=module_metadata["__version__"],
     description="MongoDB Backup and Restore Manager",
     keywords="mongodb backup restore mongodump mongorestore",
     url="https://github.com/chrisantonellis/mongobar",
