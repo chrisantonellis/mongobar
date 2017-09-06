ae90154e16c60baffdfb1ae6d45c4e6188a5cc42
Christopher Antonellis <christopher.antonellis@gmail.com>
2017-09-06 11:40:04 -0400

    adjusted imports to allow for use of __version__ instead of version in __init__

--- mongobar/__init__.py
+++ mongobar/__init__.py
@@ -1,6 +1,1 @@
-from mongobar.config import *
-from mongobar.connection import *
-from mongobar.exceptions import *
-from mongobar.mongobar import *
-
-__version__ = "0.0.4"
+from mongobar.mongobar import Mongobar
